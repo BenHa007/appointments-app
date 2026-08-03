fbeadfe8915c5c2d9b398e3e6b651b20b2ea9b5f
Benjamin <benihalperin@gmail.com>
2026-08-03 10:26:34 +0300

    feat: last one

--- backend/controllers/verifyController.js
+++ backend/controllers/verifyController.js
@@ -6,11 +6,10 @@ exports.sendCode = async (req, res) => {
   try {
     const { phone } = req.body;
 
-    // const userExists = await User.findOne({ phone });
-    // console.log('-----------------------------')
-    // if (userExists) {
-    //   return res.status(400).json({ message: 'מספר הטלפון הזה כבר רשום במערכת' });
-    // }
+    const userExists = await User.findOne({ phone });
+    if (userExists) {
+      return res.status(400).json({ message: 'מספר הטלפון הזה כבר רשום במערכת' });
+    }
 
     const code = Math.floor(1000 + Math.random() * 9000).toString();
     const hash = await bcrypt.hash(code, 10);
